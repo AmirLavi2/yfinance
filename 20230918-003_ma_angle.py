a45962786485c9a99928e0da7a6292f4956bcbfb
Amir <amir.lavi2@gmail.com>
2023-09-23 11:22:50 +0300

    init

--- 20230918-003_ma_angle.py
+++ 20230918-003_ma_angle.py
@@ -1,5 +1,9 @@
-# TODO: Build output object
 # TODO: clean code
+# TODO: Build output object
+# TODO: advance: build DB
+# TODO: split code - 1: gets API data and write to the DB
+# TODO: split code - 2: read data and calculate result
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,18 +12,22 @@ import time
 import datetime
 
 # constants
-LONG_MA = 200 #150
+MA_LONG = 150 #150
+MA_SHORT = 5 #20
 PERIOD = '1d'
 START_DATE = '2022-03-22'
 END_DATE = datetime.datetime.now() #'2023-09-22'
 INTERVAL = '1d'
+output = {}
+top_5_ma_angle_long = []
+top_5_ma_angle_short = []
+bottom_5_closest_ma_long = []
 FILE_NAME = 'tickers_200B.txt'
 # FILE_NAME = 'tickers_2B-10B.txt'
 
 # functions
 def get_stock_data(stock, startdate, enddate, period, interval):
     try:
-        ticker = stock
         yf.pdr_override()
         df = yf.download(tickers=stock, start=startdate, end=enddate, interval=interval, period=period, progress=False)
         df.reset_index(inplace=True)
@@ -28,67 +36,97 @@ def get_stock_data(stock, startdate, enddate, period, interval):
     except Exception as e:
         print(f"An error occurred while fetching data for {stock}: {str(e)}")
 
-def calc_close_moving_average_angle(data, ma):
-    ma_series = data['Close'].rolling(int(ma)).mean()
-    if ma_series.iloc[-1] > ma_series.iloc[-2]:
-        return ma_series.iloc[-1] / ma_series.iloc[-2]
-    else:
-        return -10000
+def moving_average_angle(close_price, ma):
+    ma_series = close_price.rolling(int(ma)).mean()
+    return ma_series.iloc[-1] / ma_series.iloc[-2]
 
-def closest_to_moving_average(data, ma):
-    ma_series = data['Close'].rolling(int(ma)).mean()
+def closest_to_moving_average(close_price, ma):
+    ma_series = close_price.rolling(int(ma)).mean()
     ma_value = ma_series.iloc[-1]
-    if data['Close'].iloc[-1] > ma_value:
-        return data['Close'].iloc[-1] / ma_value
-    else:
-        return 10000
+
+    return close_price.iloc[-1] / ma_value
+
+def create_output_dictionary(close_price, stock_name):
+    ma_angle_long = moving_average_angle(close_price, MA_LONG)
+    ma_angle_short = moving_average_angle(close_price, MA_SHORT)
+    closest_ma_long = closest_to_moving_average(close_price, MA_LONG)
+
+    if stock_name not in output:
+        output[stock_name] = {}
+
+    output[stock_name]['close_price'] = close_price.iloc[-1]
+
+    if ma_angle_long > 1:
+        output[stock_name]['ma_angle_long'] = ma_angle_long
+    if ma_angle_short > 1:
+        output[stock_name]['ma_angle_short'] = ma_angle_short
+    if closest_ma_long > 1:
+        output[stock_name]['closest_ma_long'] = closest_ma_long
+
+def find_top_stocks():
+
+    global top_5_ma_angle_long
+    global top_5_ma_angle_short
+    global bottom_5_closest_ma_long
+    # Iterate through the dictionary
+    for stock_symbol, data in output.items():
+        if 'ma_angle_long' in data:  # Check if the key 'ma_angle_long' exists
+            ma_angle_long = data['ma_angle_long']
+            top_5_ma_angle_long.append((stock_symbol, ma_angle_long))
+        
+        if 'ma_angle_short' in data:  # Check if the key 'ma_angle_short' exists
+            ma_angle_short = data['ma_angle_short']
+            top_5_ma_angle_short.append((stock_symbol, ma_angle_short))
+        
+        if 'closest_ma_long' in data:  # Check if the key 'closest_ma_long' exists
+            closest_ma_long = data['closest_ma_long']
+            bottom_5_closest_ma_long.append((stock_symbol, closest_ma_long))
+
+    # Sort the lists based on their respective attributes
+    top_5_ma_angle_short.sort(key=lambda x: x[1], reverse=True)
+    top_5_ma_angle_long.sort(key=lambda x: x[1], reverse=True)
+    bottom_5_closest_ma_long.sort(key=lambda x: x[1])
+
+    # Take the top 5 and bottom 5 entries for each attribute
+    top_5_ma_angle_long = top_5_ma_angle_long[:5]
+    top_5_ma_angle_short = top_5_ma_angle_short[:5]
+    bottom_5_closest_ma_long = bottom_5_closest_ma_long[:5]
+
+def print_output():
+
+    print("\nTop 5 Highest 'ma_angle_long':")
+    for i, (stock_symbol, ma_angle_long) in enumerate(top_5_ma_angle_long):
+        print(f"{i + 1}. Stock: {stock_symbol}, MA Angle long: {ma_angle_long}")
+
+    print("\nTop 5 Highest 'ma_angle_short':")
+    for i, (stock_symbol, ma_angle_short) in enumerate(top_5_ma_angle_short):
+        print(f"{i + 1}. Stock: {stock_symbol}, MA Angle short: {ma_angle_short}")
+
+    print("\n5 Lowest 'closest_ma_long':")
+    for i, (stock_symbol, closest_ma_long) in enumerate(bottom_5_closest_ma_long):
+        print(f"{i + 1}. Stock: {stock_symbol}, Closest MA long: {closest_ma_long}")
 
 def main():
     # Measure the start time
     start_time = time.time()
 
+    # read stock list
     tickers = open(FILE_NAME, 'r')
-    ticker_lines = tickers.readlines()
-    stocks_dict_min = {}
-    stocks_dict_max = {}
-
-    for line in ticker_lines:
-        df = get_stock_data(line, START_DATE, END_DATE, PERIOD, INTERVAL)
+    lines_ticker = tickers.readlines()
+    for line_stock in lines_ticker:
+        line_stock = line_stock.strip()
+        df = get_stock_data(line_stock, START_DATE, END_DATE, PERIOD, INTERVAL)
         if df is not None:
-            cma_angle = calc_close_moving_average_angle(df, LONG_MA)
-            stocks_dict_max[line] = cma_angle
+            create_output_dictionary(df['Close'], line_stock)
 
-            closest_ma = closest_to_moving_average(df, LONG_MA)
-            stocks_dict_min[line] = closest_ma
-
-    min_value = min(stocks_dict_min, key=lambda x: stocks_dict_min[x])
-    max_value = max(stocks_dict_max, key=lambda x: stocks_dict_max[x])
-
-    print(stocks_dict_min)
-    print(stocks_dict_max)
-    print(f'Closest to MA {LONG_MA}: {min_value}')
-    print(f'Highest MA {LONG_MA} angle: {max_value}')
-
-    # Sort and print top 10 minimum stocks
-    sorted_stocks_min = sorted(stocks_dict_min.items(), key=lambda x: x[1])
-    min_stocks = sorted_stocks_min[:4]
-
-    for stock, value in min_stocks:
-        print(f"Value: {value:.4f} | Stock: {stock.strip()}")
-
-    print('---------------------------------------------------')
-
-    # Sort and print top 10 maximum stocks
-    sorted_stocks_max = sorted(stocks_dict_max.items(), key=lambda x: x[1], reverse=True)
-    max_stocks = sorted_stocks_max[:4]
-
-    for stock, value in max_stocks:
-        print(f"Value: {value:.4f} | Stock: {stock.strip()}")
+    find_top_stocks()
+    print_output()
 
     # Measure the end time
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time:.4f} seconds")
+
 
 if __name__ == "__main__":
     main()
