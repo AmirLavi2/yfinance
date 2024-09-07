151919473fc9b4ca1afaf69b7e31c6f14332f13e
Amir <amir.lavi2@gmail.com>
2024-09-07 20:39:47 +0300

    db - add config.init file for connection info

--- stocks_fetch.py
+++ stocks_fetch.py
@@ -1,42 +1,63 @@
 # 1) fetch the stocks from tickers file
 # 2) insert result to DB
-from my_modules import stock_utils
-import pandas as pd
-# ----------------------------------------------------------
-def stock_check(stock_data):
-    close = stock_data['Close'].iloc[-1]
-    sma150 = stock_data['SMA150'].iloc[-1]
-    sma50 = stock_data['SMA50'].iloc[-1]
-    sma200 = stock_data['SMA200'].iloc[-1]
 
-    # find 1
-    # if stock_data['High'].iloc[-1] < stock_data['High'].iloc[-2] and stock_data['Low'].iloc[-1] > stock_data['Low'].iloc[-2]:
-    #     if stock_data['High'].iloc[-2] > stock_data['High'].iloc[-3] and stock_data['Low'].iloc[-2] < stock_data['Low'].iloc[-3]:
-    #         if close < 50:
-    #             print(stock_data.ticker)
-    # print(stock_data)
-    # print('###############################')
+from my_modules import stock_utils
+import configparser
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+import sys
+import re
 # ----------------------------------------------------------
+# Fetch stock tickers from the file
 with open('tickers/tickers_from_1B.txt', 'r') as file:
     stocks_tickers = [line.strip() for line in file]
-# ----------------------------------------------------------
-from sqlalchemy import create_engine
 
-# MySQL connection parameters
+# ----------------------------------------------------------
+# Read database configuration from config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Extract database connection info from the config file
 db_config = {
-    'host': '127.0.0.1',
-    'user': 'amir',
-    'password': '5590',
-    'database': 'stocks'
+    'host': config['database']['host'],
+    'user': config['database']['user'],
+    'password': config['database']['password'],
+    'database': config['database']['database']
 }
 
-# Create a SQLAlchemy engine
-engine = create_engine('mysql+mysqlconnector://{user}:{password}@{host}/{database}'.format(**db_config))
+# Create a SQLAlchemy engine using the configuration from the .ini file
+try:
+    engine = create_engine(
+        'mysql+mysqlconnector://{user}:{password}@{host}/{database}'.format(**db_config)
+    )
+    # Test the connection to ensure it's established correctly
+    with engine.connect() as conn:
+        print("Connection successful. fetching tickers data")
+except SQLAlchemyError as e:
+    err = str(e)
+    match = re.search(r'\d{4} \(\d{5}\): (.*)', err)
+    if match:
+        err_short = match.group(1)
+        print("DB Error: ", err_short)
+    else:
+        print("DB Error: ", err)
+
+    sys.exit(1)
 
 # ----------------------------------------------------------
-# Loop through all tickers and plot them on the same axes
+# Loop through all tickers, fetch stock data, and insert into the database
 for ticker in stocks_tickers:
     stock_data = stock_utils.stock_get(ticker)
     if stock_data is not False:
-        stock_check(stock_data)
-        stock_data.to_sql('stocks', con=engine, if_exists='append', index=True)
+        try:
+            stock_data.to_sql('stocks', con=engine, if_exists='append', index=True)
+        except SQLAlchemyError as e:
+            err = str(e)
+            match = re.search(r'\d{4} \(\d{5}\): (.*)', err)
+            if match:
+                err_short = match.group(1)
+                print(f"Insert '{ticker}' data Error: {err_short}")
+            else:
+                print(f"Insert '{ticker}' data Error: {err}")
+# ----------------------------------------------------------
+print("ticker data is now up to date")
